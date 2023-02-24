0ba6c1fb715a1b049075b0dd65301be27c1d1965
Luc Van Minh <luk.mink@gmail.com>
2023-02-24 16:10:09 +0700

    adding migration

--- hasura/seeds/W2DB/1676256116499_defaultUserSeed.sql
+++ hasura/seeds/W2DB/1676256116499_defaultUserSeed.sql
@@ -1,3 +1,3 @@
 SET check_function_bodies = false;
-INSERT INTO public."User" (id, "createdAt", "updatedAt", email, password, firstname, lastname, "organizationId", role, "avatarId", "googleId", "googleToken", provider) VALUES (5, '2023-02-10 10:01:43.843', '2023-02-10 10:01:43.843', 'admin@gmail.com', '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', 'Lisa', 'Simpson', 4, 'USER', NULL, NULL, NULL, 'local');
+INSERT INTO public."User" (id, "createdAt", "updatedAt", email, password, firstname, lastname, "organizationId", role, "avatarId", "googleId", "googleToken", provider) VALUES (5, '2023-02-10 10:01:43.843', '2023-02-10 10:01:43.843', 'admin@gmail.com', '$2b$10$EpRnTzVlqHNP0.fUbXUwSOyuiXe/QLSUG6xNekdHgTGmrpHEfIoxm', 'Lisa', 'Simpson', NULL, 'USER', NULL, NULL, NULL, 'local');
 SELECT pg_catalog.setval('public."User_id_seq"', 5, true);
