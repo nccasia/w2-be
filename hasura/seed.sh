b0e2c83a42206eddb1e9840c383d10abf6e69b99
Luc Van Minh <luk.mink@gmail.com>
2023-02-27 15:42:23 +0700

    update seed

--- hasura/seed.sh
+++ hasura/seed.sh
@@ -2,8 +2,8 @@ rm -rf ./seeds/W2DB
 
 hasura seed create defaultOrganization --database-name W2DB --from-table Organization
 hasura seed create defaultUser --database-name W2DB --from-table User
-hasura seed create defaultPermission --database-name W2DB --from-table Permission
 hasura seed create defaultPermissionGroup --database-name W2DB --from-table PermissionGroup
+hasura seed create defaultPermission --database-name W2DB --from-table Permission
 hasura seed create defaultUserPermission --database-name W2DB --from-table UserPermission
 hasura seed create defaultTeam --database-name W2DB --from-table Team
 hasura seed create defaultTaskDefinition --database-name W2DB --from-table TaskDefinition
